a194b1e7d4efd6c016ba8a99a0fa3d7082071382
gitguudad <55541178+gitguudad@users.noreply.github.com>
2019-09-26 08:50:52 -0400

    Add files via upload

---
+++ FootballFun.java
@@ -0,0 +1,50 @@
+/*
+ * IS4010 Fal 2019
+ * Assignment 05
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+package footballFun;
+
+public class FootballFun {
+
+	public static void main(String[] args) {
+		int correctCount = 0;
+		if (translateScore(2).equals("safety") == true) { correctCount++;}
+		if (translateScore(3).equals("field goal") == true) { correctCount++;}
+		if (translateScore(6).equals("touchdown") == true) { correctCount++;}
+		if (translateScore(7).equals("touchdown and extra point") == true) { correctCount++;}
+		if (translateScore(8).equals("touchdown and 2-point conversion") == true) { correctCount++;}
+		if (translateScore(-1).equals("") == true) { correctCount++;}
+		if (translateScore(1).equals("invalid") == true) { correctCount++;}
+		if (translateScore(10).equals("you must be playing Quidditch ") == true) { correctCount++;}
+		
+		if (correctCount == 8) {
+			System.out.println("All tests passed");
+		} else {
+			System.out.println("At least one test failed");
+		}
+	}
+	public static String translateScore (int score) {
+		if (score == 2) {
+			return "safety";			//checking for safety score
+		} else if (score == 3){
+			return "field goal";		//checking for field goal score
+		} else if (score == 6) {
+			return "touchdown";			//checking for touchdown score
+		}else if (score == 7) {
+			return "touchdown and extra point";	//checking for touchdown and extra point
+		}else if (score == 8) {
+			return "touchdown and 2-point conversion"; //checking for touchdown and 2-point conversion
+		} else if (score == -1) {
+			return ""; 		//checking for negatives
+		}else if (score == 1) {
+			return "invalid";		//checking for invalid numbers 
+		} else if (score == 10) {
+			return "you must be playing Quidditch ";	//checking for accurate sport scores
+		}
+		return "";
+		
+		
+	}
+}
